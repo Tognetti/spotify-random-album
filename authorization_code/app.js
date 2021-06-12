6ae079950f27336188d2b21afc7a71fcd6135a3c
Adolfo <adolfotognetti@gmail.com>
2021-06-12 00:54:35 -0300

    Fix scopes

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -39,7 +39,7 @@ app.get('/login', function(req, res) {
   res.cookie(stateKey, state);
 
   // your application requests authorization
-  var scope = 'user-read-private user-read-email user-library-read';
+  var scope = 'user-library-read';
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
